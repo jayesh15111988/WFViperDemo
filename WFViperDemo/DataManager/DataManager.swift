1a43cab8120c0509e75eafd189edda85c8b8ea8e
Jayesh Kawli <jkawli@wayfair.com>
2017-08-13 14:47:54 -0400

    Removed stub test methods

--- WFViperDemo/DataManager/DataManager.swift
+++ WFViperDemo/DataManager/DataManager.swift
@@ -34,7 +34,7 @@ class DataManager: DataDownloader {
     func downloadItemsFor(name: String) {
         let when = DispatchTime.now() + 0.5
         DispatchQueue.main.asyncAfter(deadline: when) {
-            self.itemsDetailsInteractor?.detailsForUserFetched(result: ["jay", "j", "kaw", "don", "ket"])
+            self.itemsDetailsInteractor?.detailsForUserFetched(result: ["A", "B", "C", "D", "E"])
         }
     }
 }
